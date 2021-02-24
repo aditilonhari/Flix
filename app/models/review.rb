4aa1736d6a665073eceb14b0c9923da9cf2bd267
Aditi <alonhari@newrelic.com>
2021-02-23 18:39:56 -0800

    Ready to deploy v1

---
+++ app/models/review.rb
@@ -0,0 +1,20 @@
+class Review < ApplicationRecord
+  belongs_to :movie
+  belongs_to :user
+
+  validates :comment, length: { minimum: 4 }
+
+  STARS = [1, 2, 3, 4, 5]
+
+  validates :stars, inclusion: {
+    in: STARS,
+    message: "must be between 1 and 5"
+  }
+  scope :past_n_days, ->(days) { where("created_at >= ?" , days.days.ago) }
+  scope :grossed_less_than, ->(amount) { released.where("total_gross < ?", amount) }
+  scope :grossed_greater_than, ->(amount) { released.where("total_gross > ?", amount) }
+
+  def stars_as_percent
+    (stars / 5.0) * 100.0
+  end
+end
